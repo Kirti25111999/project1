876ec16a60ca3242240147931686621c4e8d190f
Kirti25111999 <100936874+Kirti25111999@users.noreply.github.com>
2022-12-02 16:11:24 +0530

    Update Mainclass1.java

--- Mainclass1.java
+++ Mainclass1.java
@@ -7,11 +7,11 @@ public class Mainclass1 {
 	public static void main(String[] args) {
 		
 		Student s1=new Student();
-		s1.name="Sakshi";
+		s1.name="Kirti";
 		s1.age=22;
 		
 		Student s2=new Student();
-		s2.name="Sahil";
+		s2.name="Pooja";
 		s2.age=24;
 		
 		System.out.println(s1.name+" "+s1.age);
